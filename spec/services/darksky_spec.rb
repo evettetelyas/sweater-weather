ec68a5d29dd7e90aaaad16520ee50775b79053da
Evette Telyas <etelyas@gmail.com>
2019-11-03 16:14:00 -0700

    remove migrations and add service specs

---
+++ spec/services/darksky_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "darkstar api service" do
+	it "should return a forecast for a city" do
+		service = DarkskyApiService.new
+		data = service.forecast_data("39.7392,-104.9903")
+
+		expect(data).to have_key(:latitude)
+		expect(data).to have_key(:longitude)
+		expect(data).to have_key(:currently)
+		expect(data).to have_key(:hourly)
+		expect(data).to have_key(:daily)
+	end
+end
---
+++ spec/services/flickr_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "flickr api service" do
+	it "should return an image for a city" do
+		service = FlickrService.new
+		data = service.image_data("denver", "co", "39.7392","-104.9903")
+
+		expect(data[:photos][:photo][0]).to have_key(:title)
+		expect(data[:photos][:photo][0]).to have_key(:server)
+		expect(data[:photos][:photo][0]).to have_key(:id)
+		expect(data[:photos][:photo][0]).to have_key(:secret)
+	end
+end
