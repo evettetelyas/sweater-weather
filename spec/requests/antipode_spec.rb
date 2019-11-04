61b8ca9d6a97d46fd8568047aefeefe9ba9811a5
Evette Telyas <etelyas@gmail.com>
2019-11-04 10:22:02 -0700

    update antipode specs

---
+++ spec/requests/antipode_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "antipode spec" do
+	it "should show antipode city data for a city" do
+
+		get "/api/v1/antipode?location=hongkong"
+
+		data = JSON.parse(response.body, symbolize_names: true)
+
+		expect(data[:data][:attributes]).to have_key(:location_name)
+		expect(data[:data][:attributes]).to have_key(:forecast)
+		expect(data[:data][:attributes]).to have_key(:search_location)
+	end
+end
