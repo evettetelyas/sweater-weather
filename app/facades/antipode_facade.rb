9fc7c3dc64a39d01f5a6f15ea6278e7aeb44a28a
Evette Telyas <etelyas@gmail.com>
2019-11-04 09:35:09 -0700

    find current weather for antipode

--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -11,8 +11,30 @@ class AntipodeFacade
 		get_coords[:results][0][:geometry][:location]
 	end
 
-	def antipode_data
-		binding.pry
+	def antipode_svc_data
 		AntipodeService.new.antipode_data(lat_lng[:lat], lat_lng[:lng])
+	end
+
+	def antipode_coords
+		antipode_svc_data[:data][:attributes]
+	end
+
+	def format_lat_lng
+		antipode_coords[:lat].round(4).to_s + "," + antipode_coords[:long].round(4).to_s
+	end
+
+	def darksky_service
+		DarkskyApiService.new
+	end
+
+	def forecast_data
+		darksky_service.forecast_data(format_lat_lng)
+	end
+
+	def current_weather
+		forecast = Forecast.new(forecast_data)
+		forecast.create_hourly(forecast_data[:hourly][:data], forecast.timezone)
+		forecast.create_daily(forecast_data[:daily][:data])
+		forecast
 	end
 end
--- app/services/antipode_service.rb
+++ app/services/antipode_service.rb
@@ -1,16 +1,16 @@
 class AntipodeService
 
-	def antipode_data(lat_lng)
+	def antipode_data(lat, lng)
 		data = conn.get('antipodes') do |req|
 			req.params[:lat] = lat
-			req.params[:lng] = lng
+			req.params[:long] = lng
 		end
 		JSON.parse(data.body, symbolize_names: true)
 	end
 
 	private
 	def conn
-		Faraday.new(url: "http://amypode.herokuapp.com/api/v1/antipodes?lat=27&long=-82") do |f|
+		Faraday.new(url: "http://amypode.herokuapp.com/api/v1/") do |f|
 		  f.adapter  Faraday.default_adapter
 		  f.headers[:api_key] = ENV["ANTIPODE_API_KEY"]
 		end
